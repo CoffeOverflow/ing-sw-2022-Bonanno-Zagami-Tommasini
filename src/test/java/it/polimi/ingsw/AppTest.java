3abbbaf4ac439aad70af6a8ecdb156b85cf08b7e
Angelo Zagami <angelo.zagami@mail.polimi.it>
2022-03-15 15:36:06 +0100

    Uploaded project files

---
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw;
+
+//import static org.junit.Assert.assertTrue;
+//import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
