7f7a9474fe1e35f325aa66185933f33e3fdb10cb
federicatommasini <federica.tommasini@mail.polimi.it>
2022-04-20 01:11:47 +0200

    added comments

--- src/main/java/it/polimi/ingsw/Model/Effect.java
+++ src/main/java/it/polimi/ingsw/Model/Effect.java
@@ -1,20 +1,41 @@
 package it.polimi.ingsw.Model;
 
+/**
+ * interface Effect
+ * @author Federica Tommasini
+ */
 public interface Effect {
 
+    /**
+     * execute the effect of the character card
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     void effect(Player player, Island island, CharacterCard card);
 }
 
 class Effect1 implements Effect{
 
-    public void effect(Player player, Island island, CharacterCard card){//carta che ha 4 studenti e il giocatore ne sceglie uno da mettere su un'isola
+    /**
+     * place one student, chosen by the player out of the four of the card, on the specified island
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
+    public void effect(Player player, Island island, CharacterCard card){
         island.addStudents(card.chosenStudents.get().keySet().iterator().next(),1);
     }
 }
 
 class Effect2 implements Effect{
 
-    //calcola maggioranza su isola scelta come se madre natura ci fosse finita sopra,
+    /**
+     * compute the influence on the island as if mother nature ended up on it
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
         int max=0;
         int n=0;
@@ -32,7 +53,12 @@ class Effect2 implements Effect{
 }
 class Effect3 implements Effect{
 
-    //puoi muovere madre natura fino a due passi in più rispetto alla tua carta assistente
+    /**
+     * move mother nature at most of two additional steps with respect to the value of the assistant card
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
@@ -40,7 +66,13 @@ class Effect3 implements Effect{
 
 class Effect4 implements Effect{
 
-    //piazza una tessera divieto su isola scelta, la prima volta che madre natura ci passainfluenza non calcolata
+    /**
+     * place a noEntryCard on the island, so that the first time that mother nature
+     * ends up on it, the influence is not calculated
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
         island.setNoEntryCard(true);
     }
@@ -48,7 +80,12 @@ class Effect4 implements Effect{
 
 class Effect5 implements Effect{
 
-    //durante il conteggio di un'influenza su un'isola le torri non vengono calcolate
+    /**
+     * during the computation of the influence of the island the number of towers is not considered
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
@@ -56,14 +93,24 @@ class Effect5 implements Effect{
 
 class Effect6 implements Effect{
 
-    //carta che ha 6 studenti, il giocatore ne può prendere al max 3 e scambiarli con 3 presenti nel suo ingresso
+    /**
+     * switch at most three students, among the six of the card, with students in the school entrance
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
         //TODO trovare un modo per passargli anche gli studenti da togliere dall'ingresso della scuola come parametro
     }
 }
 class Effect7 implements Effect{
 
-    //due punti influenza addizionali nel calcolo dell'influenza
+    /**
+     * assign two additional points to the player when computing the influence on the island
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
@@ -71,7 +118,13 @@ class Effect7 implements Effect{
 
 class Effect8 implements Effect{
 
-    //scegli un colore di studente che in quel turno non cointribuisce al calcolo dell'influenza
+    /**
+     * the player choose a color that is not considered in the computation of the influence
+     * of the island in that turn
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
@@ -79,7 +132,12 @@ class Effect8 implements Effect{
 
 class Effect9 implements Effect{
 
-    //puoi scambiare fino a due studenti tra sala e ingresso
+    /**
+     * switch at most two students of the dining room with students of the entrance of the school
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
@@ -87,7 +145,12 @@ class Effect9 implements Effect{
 
 class Effect10 implements Effect{
 
-    //carta che ha 4 studenti, il giocatore ne può scegliere uno da mettere nella sua sala
+    /**
+     * place the student chosen by the player from the four of the card on his dining room
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
         //int n=player.getStudentsOf(card.chosenStudents.get().keySet().iterator().next());
         //player.setStudents(card.chosenStudents.get().keySet().iterator().next(),n+1);
@@ -96,7 +159,13 @@ class Effect10 implements Effect{
 
 class Effect11 implements Effect{
 
-    //scegli un colore di studente, tutti (incluso te) rimetteranno tre studenti di quel colore presenti nella loro sala nel sacchetto
+    /**
+     * All the players put three students of the chosen color, placed in their dining room,
+     * back into the bag
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
@@ -104,8 +173,14 @@ class Effect11 implements Effect{
 
 class Effect12 implements Effect{
 
-    //durante questo turno prendi il controllo dei professori anche se nella tua sala hai lo stesso numero di studenti
-    //del giocatore che li controlla in quel momento
+    /**
+     * during the current turn, the player takes control of the professors even if he
+     * has the same number of students in his dining room of the player who is currently
+     * controlling such professors
+     * @param player player who uses the character card
+     * @param island island to which the changes will be applied
+     * @param card character card that is used
+     */
     public void effect(Player player, Island island, CharacterCard card){
 
     }
