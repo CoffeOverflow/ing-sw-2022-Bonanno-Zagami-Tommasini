69abfdca29308f4477f072683f348e39fc9ba3fd
federicatommasini <federica.tommasini@mail.polimi.it>
2022-04-20 01:11:47 +0200

    added comments

--- src/main/java/it/polimi/ingsw/Model/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/Model/CharacterCard.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.Model;
 import java.util.EnumMap;
 import java.util.Optional;
 
+/**
+ * class CharacterCard
+ * @author Federica Tommasini
+ */
 public class CharacterCard {
     int cost;
     String asset;
@@ -10,14 +14,22 @@ public class CharacterCard {
     Optional<EnumMap<Color, Integer>> students;
     Optional<EnumMap<Color, Integer>> chosenStudents;
 
-    //costruttore per le carte personaggio che hanno degli studenti sopra
-    public CharacterCard(int cost, String asset, EnumMap<Color, Integer> students) {
+    /**
+     * constructor for the character cards that have some students placed on them
+     * @param cost cost that has to be paid to use the card
+     * @param asset name of the file corresponding to the card
+     * @param students contains the student placed on the card, mapping their color with their quantity
+     * @throws IllegalArgumentException if the number of students is wrong
+     * @throws IllegalStateException if the card doesn't contain students
+     */
+    public CharacterCard(int cost, String asset, EnumMap<Color, Integer> students)
+            throws IllegalArgumentException, IllegalStateException{
 
         this.cost = cost;
         this.asset = asset;
 
         switch (asset) {
-            case "CarteTOT_front": //carta che ha 4 studenti e il giocatore ne sceglie uno da mettere su un'isola
+            case "CarteTOT_front":
                 if (students!=null && students.size() == 4) {
                     this.students.of(students);
                 }else{
@@ -25,7 +37,7 @@ public class CharacterCard {
                 }
                 effect = new Effect1();
                 break;
-            case "CarteTOT_front6": //carta che ha 6 studenti, il giocatore ne può prendere al max 3 e scambiarli con 3 presenti nel suo ingresso
+            case "CarteTOT_front6":
                 if (students!=null && students.size() == 6){
                     this.students.of(students);
                 }
@@ -34,7 +46,7 @@ public class CharacterCard {
                 }
                 effect = new Effect6();
                 break;
-            case "CarteTOT_front10": //carta che ha 4 studenti, il giocatore ne può scegliere uno da mettere nella sua sala
+            case "CarteTOT_front10":
                 if (students!=null && students.size() == 4){
                     this.students.of(students);
                 }
@@ -49,6 +61,16 @@ public class CharacterCard {
     }
 
 
+    /**
+     * constructor for the character cards that don't have students on them
+     * @param cost cost cost that has to be paid to use the card
+     * @param asset name of the file corresponding to the card
+     */
+    public CharacterCard(int cost, String asset){
+        this.cost = cost;
+        this.asset = asset;
+    }
+
     public int getCost(){
         return cost;
     }
@@ -61,7 +83,12 @@ public class CharacterCard {
         this.students.of(students);
     }
 
-    public void setChosenStudents(EnumMap<Color, Integer> chosenStudents) {
+    /**
+     * set the students that the player chose to take from the character card
+     * @param chosenStudents students taken from the character card
+     * @throws IllegalStateException if the card doesn't contain students or if the number of chosen student is wrong
+     */
+    public void setChosenStudents(EnumMap<Color, Integer> chosenStudents) throws IllegalStateException{
         if( (this.asset.equals("CarteTOT_front1") && chosenStudents.size()==1) ||
             (this.asset.equals("CarteTOT_front6") && chosenStudents.size()<=3) ||
             (this.asset.equals("CarteTOT_front10") && chosenStudents.size()==1)
@@ -72,6 +99,11 @@ public class CharacterCard {
         }
     }
 
+    /**
+     * execute the effect of the character card
+     * @param player player who uses the card
+     * @param island island on which the changes will be applied
+     */
     public void useCard(Player player, Island island){
         effect.effect(player,island, this);
         if(chosenStudents.isPresent()){
