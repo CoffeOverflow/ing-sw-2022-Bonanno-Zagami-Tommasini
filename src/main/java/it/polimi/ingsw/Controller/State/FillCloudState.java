5666f67cde31860172bcf1b4296ccb5751435def
CoffeOverflow <angelo.zagami@mail.polimi.it>
2022-04-25 17:29:56 +0200

    Added playerID in Player. Removed CheckEndOfGame and FillCloudState States and completed method cheEndGame in GameController.

--- src/main/java/it/polimi/ingsw/Controller/State/FillCloudState.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.Controller.State;
-
-import it.polimi.ingsw.Controller.Action;
-import it.polimi.ingsw.Controller.GameController;
-
-public class FillCloudState implements GameControllerState{
-    @Override
-    public void turnAction(GameController gc, Action action){}
-}
