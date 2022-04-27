efb8af1eeb3c49fd4cb7821a7443ed8d641edc7a
federicatommasini <federica.tommasini@mail.polimi.it>
2022-04-27 13:53:03 +0200

    added messages

--- src/main/java/it/polimi/ingsw/Server/ServerToClient/MatchCreated.java
+++ src/main/java/it/polimi/ingsw/Server/ServerToClient/MatchCreated.java
@@ -8,51 +8,26 @@ import java.util.List;
 
 public class MatchCreated implements ServerToClientMessage{
 
-    private List<String> playerNicknames;
 
-    private int numberOfPlayers;
-
-    private boolean expertMode;
-
-    private HashMap<String,String> mapPlayerWizard;
 
     private int motherNaturePosition;
 
     private HashMap<Integer, Color> mapStudentIsland; //at the beginning one student put on each island
 
-    private HashMap<Integer, Tower> mapTowerToPlayer;
 
-    //maps a player with the students placed in his entrance (each student mapped to the number)
-    private HashMap<Integer,HashMap<Color,Integer>> mapPlayerEntranceStudent;
 
-    private List<String> characterCards;
+    public MatchCreated(int motherNaturePosition, HashMap<Integer, Color> mapStudentIsland) {
 
-    public MatchCreated(List<String> playerNicknames, int numberOfPlayers, boolean expertMode, HashMap<String,
-            String> mapPlayerWizard, int motherNaturePosition, HashMap<Integer, Color> mapStudentIsland, HashMap<Integer,
-            Tower> mapTowerToPlayer, HashMap<Integer, HashMap<Color, Integer>> mapPlayerEntranceStudent) {
-        this.playerNicknames = playerNicknames;
-        this.numberOfPlayers = numberOfPlayers;
-        this.expertMode = expertMode;
-        this.mapPlayerWizard = mapPlayerWizard;
         this.motherNaturePosition = motherNaturePosition;
         this.mapStudentIsland = mapStudentIsland;
-        this.mapTowerToPlayer = mapTowerToPlayer;
-        this.mapPlayerEntranceStudent = mapPlayerEntranceStudent;
 
     }
 
-    public MatchCreated(List<String> playerNicknames, int numberOfPlayers, boolean expertMode, HashMap<String,
-            String> mapPlayerWizard, int motherNaturePosition, HashMap<Integer, Color> mapStudentIsland, HashMap<Integer,
-            Tower> mapTowerToPlayer, HashMap<Integer, HashMap<Color, Integer>> mapPlayerEntranceStudent, List<String> characterCards) {
-        this.playerNicknames = playerNicknames;
-        this.numberOfPlayers = numberOfPlayers;
-        this.expertMode = expertMode;
-        this.mapPlayerWizard = mapPlayerWizard;
-        this.motherNaturePosition = motherNaturePosition;
-        this.mapStudentIsland = mapStudentIsland;
-        this.mapTowerToPlayer = mapTowerToPlayer;
-        this.mapPlayerEntranceStudent = mapPlayerEntranceStudent;
-        this.characterCards=characterCards;
+    public int getMotherNaturePosition() {
+        return motherNaturePosition;
+    }
 
+    public HashMap<Integer, Color> getMapStudentIsland() {
+        return mapStudentIsland;
     }
 }
---
+++ src/main/java/it/polimi/ingsw/Server/ServerToClient/SetUpCharacterCard.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.Server.ServerToClient;
+
+import it.polimi.ingsw.Client.ClientToServer.ClientToServerMessage;
+import it.polimi.ingsw.Model.Color;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class SetUpCharacterCard implements ClientToServerMessage{
+
+    private String[] characterCards;
+
+    private HashMap<Color, Integer> firstCardStudents;
+
+    private HashMap<Color, Integer> secondCardStudents;
+
+    private HashMap<Color, Integer> thirdCardStudents;
+
+    public SetUpCharacterCard(String[] characterCards) {
+        this.characterCards = characterCards;
+    }
+
+    public void setFirstCardStudents(HashMap<Color, Integer> firstCardStudents) {
+        this.firstCardStudents = firstCardStudents;
+    }
+
+    public void setSecondCardStudents(HashMap<Color, Integer> secondCardStudents) {
+        this.secondCardStudents = secondCardStudents;
+    }
+
+    public void setThirdCardStudents(HashMap<Color, Integer> thirdCardStudents) {
+        this.thirdCardStudents = thirdCardStudents;
+    }
+
+    public String[] getCharacterCards() {
+        return characterCards;
+    }
+
+    public HashMap<Color, Integer> getFirstCardStudents() {
+        return firstCardStudents;
+    }
+
+    public HashMap<Color, Integer> getSecondCardStudents() {
+        return secondCardStudents;
+    }
+
+    public HashMap<Color, Integer> getThirdCardStudents() {
+        return thirdCardStudents;
+    }
+}
