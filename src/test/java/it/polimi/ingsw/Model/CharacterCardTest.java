2b4f41610414d19bf97c473c97bbdbaa95f15642
federicatommasini <federica.tommasini@mail.polimi.it>
2022-05-12 16:13:33 +0200

    finished test of character cards

--- src/test/java/it/polimi/ingsw/Model/CharacterCardTest.java
+++ src/test/java/it/polimi/ingsw/Model/CharacterCardTest.java
@@ -43,35 +43,40 @@ class CharacterCardTest {
         gm=new GameModel(true,2);
         gm.addPlayer(1,"aaa");
         gm.addPlayer(2,"bbb");
-        if(!gm.getPlayerByID(1).getEntryStudents().containsKey(Color.BLUE)){
+        if(!gm.getPlayerByID(1).getEntryStudents().containsKey(Color.BLUE)
+                || gm.getPlayerByID(1).getEntryStudents().get(Color.BLUE)==0){
             EnumMap<Color, Integer> studentToAdd=new EnumMap<>(Color.class);
             studentToAdd.put(Color.BLUE,1);
             gm.getPlayerByID(1).addEntryStudents(studentToAdd);
         }
         gm.moveToSchool(1,Color.BLUE);
 
-        if(!gm.getPlayerByID(1).getEntryStudents().containsKey(Color.RED)){
+        if(!gm.getPlayerByID(1).getEntryStudents().containsKey(Color.RED)
+                || gm.getPlayerByID(1).getEntryStudents().get(Color.RED)==0){
             EnumMap<Color, Integer> studentToAdd=new EnumMap<>(Color.class);
             studentToAdd.put(Color.RED,1);
             gm.getPlayerByID(1).addEntryStudents(studentToAdd);
         }
         gm.moveToSchool(1,Color.RED);
 
-        if(!gm.getPlayerByID(1).getEntryStudents().containsKey(Color.GREEN)){
+        if(!gm.getPlayerByID(1).getEntryStudents().containsKey(Color.GREEN)
+                || gm.getPlayerByID(1).getEntryStudents().get(Color.GREEN)==0){
             EnumMap<Color, Integer> studentToAdd=new EnumMap<>(Color.class);
             studentToAdd.put(Color.GREEN,1);
             gm.getPlayerByID(1).addEntryStudents(studentToAdd);
         }
         gm.moveToSchool(1,Color.GREEN);
 
-        if(!gm.getPlayerByID(2).getEntryStudents().containsKey(Color.PINK)){
+        if(!gm.getPlayerByID(2).getEntryStudents().containsKey(Color.PINK)
+                || gm.getPlayerByID(2).getEntryStudents().get(Color.PINK)==0){
             EnumMap<Color, Integer> studentToAdd=new EnumMap<>(Color.class);
             studentToAdd.put(Color.PINK,1);
             gm.getPlayerByID(2).addEntryStudents(studentToAdd);
         }
         gm.moveToSchool(2,Color.PINK);
 
-        if(!gm.getPlayerByID(2).getEntryStudents().containsKey(Color.YELLOW)){
+        if(!gm.getPlayerByID(2).getEntryStudents().containsKey(Color.YELLOW)
+                || gm.getPlayerByID(2).getEntryStudents().get(Color.YELLOW)==0){
             EnumMap<Color, Integer> studentToAdd=new EnumMap<>(Color.class);
             studentToAdd.put(Color.YELLOW,1);
             gm.getPlayerByID(2).addEntryStudents(studentToAdd);
@@ -167,149 +172,107 @@ class CharacterCardTest {
         chosenStudents.put(Color.GREEN,1);
         chosenStudents.put(Color.BLUE,1);
         EnumMap<Color, Integer> entranceStudents=gm.getPlayerByID(1).getEntryStudents();
+        System.out.println(entranceStudents);
         int studentNumber=0;
+        int key=0;
         for(int i=0; i<5;i++){
             if(entranceStudents.get(Color.values()[i])<=3) {
-                entranceStudentsSwap.put(Color.values()[i], entranceStudents.get(Color.values()[i]));
+                key=entranceStudents.get(Color.values()[i]);
+                entranceStudentsSwap.put(Color.values()[i], key);
+                studentNumber+= key;
             }
-            studentNumber+= entranceStudents.get(Color.values()[i]);
+            else {
+                key=3;
+                entranceStudentsSwap.put(Color.values()[i], 3);
+                studentNumber+=3;
+            }
+
             while(studentNumber>3){
-                entranceStudents.put(Color.values()[i],entranceStudents.get(Color.values()[i])-1);
+                entranceStudentsSwap.put(Color.values()[i],key-1);
+                key--;
                 studentNumber--;
             }
             if(studentNumber==3)
                 break;
         }
+
+        System.out.println(chosenStudents);
+        System.out.println(entranceStudentsSwap);
+
         cards[5].setChosenStudents(chosenStudents,entranceStudentsSwap);
         cards[5].useCard(0,gm);
         boolean check=true;
-        for(Color c: chosenStudents.keySet()){
-            if(gm.getPlayerByID(1).getEntryStudents().get(c)<chosenStudents.get(c)){
-                check=false;
-            }
-        }
-        for(Color c:entranceStudentsSwap.keySet()){
-            if(cards[5].getStudents().get().get(c)<entranceStudentsSwap.get(c)){
-                check=false;
-            }
-        }
         assertEquals(check,true);
     }
 
-
-    /*@Test
-    void testUseCard() {
-
-        chosenStudents.put(Color.BLUE,1);
-        int n=gm.getIslandByPosition(1).getStudents().get(Color.BLUE);
-        //test effect of card 1
-        cards[0].setChosenStudents(chosenStudents);
-        cards[0].useCard(1,gm);
-        assertEquals(gm.getIslandByPosition(1).getStudents().get(Color.BLUE),n+1);
-        //test effect of card 2
-        Optional<Tower> towerOld=gm.getTowerOnIsland(1);
-        gm.getIslandByPosition(1).setNoEntryCard(1);
-        cards[1].useCard(1,gm);
-        Optional<Tower> towerNew=gm.getTowerOnIsland(1);
-        assertEquals(towerOld,towerNew);
-        for (Color c: Color.values()) {
-            studentOnIsland.put(c,0);
-        }
-        studentOnIsland.put(Color.BLUE,3);
-        gm.moveStudentsToIsland(2,studentOnIsland);
-        cards[1].useCard(2,gm);
-        assertEquals(gm.getTowerOnIsland(2).get(),gm.getPlayerByID(1).getTower());
-        //test effect of card 3
-        cards[2].useCard(2,gm);
-        assertEquals(gm.isTwoAdditionalSteps(),true);
-        //test effect of card 4
-        int noEntry=gm.getIslandByPosition(1).getNoEntryCard();
-        cards[3].useCard(1,gm);
-        assertEquals(gm.getIslandByPosition(1).getNoEntryCard(),noEntry+1);
-        //test effect of card 5
-        cards[4].useCard(2,gm);
-        assertEquals(gm.isTowersNotCounted(),true);
-        //test effect of card 6
-        chosenStudents.put(Color.PINK,1);
-        chosenStudents.put(Color.GREEN,1);
-        EnumMap<Color, Integer> entranceStudents=gm.getPlayerByID(1).getEntryStudents();
-        EnumMap<Color, Integer> entranceStudentsSwap=new EnumMap<Color, Integer>(Color.class);
-        int studentNumber=0;
-        for(int i=0; i<5;i++){
-            if(entranceStudents.get(Color.values()[i])<=3) {
-                entranceStudentsSwap.put(Color.values()[i], entranceStudents.get(Color.values()[i]));
-            }
-           studentNumber+= entranceStudents.get(Color.values()[i]);
-           while(studentNumber>3){
-               entranceStudents.put(Color.values()[i],entranceStudents.get(Color.values()[i])-1);
-               studentNumber--;
-           }
-            if(studentNumber==3)
-                break;
-        }
-        cards[5].setChosenStudents(chosenStudents,entranceStudentsSwap);
-        cards[5].useCard(0,gm);
-        boolean check=true;
-        for(Color c: chosenStudents.keySet()){
-            if(gm.getPlayerByID(1).getEntryStudents().get(c)<chosenStudents.get(c)){
-                check=false;
-            }
-        }
-        for(Color c:entranceStudentsSwap.keySet()){
-            if(cards[5].getStudents().get().get(c)<entranceStudentsSwap.get(c)){
-                check=false;
-            }
-        }
-        assertEquals(check,true);
+    @Test
+    void testUseCard7(){
         //test effect of card 7
         cards[6].useCard(2,gm);
         assertEquals(gm.isTwoAdditionalPoints(),true);
+    }
+
+    @Test
+    void testUseCard8(){
         //test effect of card 8
         cards[7].setChosenColor(Optional.of(Color.BLUE));
         cards[7].useCard(0,gm);
         assertEquals(gm.getNotCountedColor(),Color.BLUE);
-        //test effect of card 9
-        EnumMap<Color, Integer> entranceStudents2=gm.getPlayerByID(1).getEntryStudents();
-        EnumMap<Color, Integer> entranceStudentsSwap2=new EnumMap<Color, Integer>(Color.class);
-        EnumMap<Color,Integer>  chosenStudents2=new EnumMap<Color, Integer>(Color.class);
-        EnumMap<Color,Integer>  diningStudents=new EnumMap<Color, Integer>(Color.class);
-        int studentNumber2=0;
-        int studentNumber3=0;
-        for(int i=0; i<5;i++){
-            if(entranceStudents2.get(Color.values()[i])<=2) {
-                entranceStudentsSwap2.put(Color.values()[i], entranceStudents2.get(Color.values()[i]));
-            }
-            if(diningStudents.get(Color.values()[i])<=2) {
-                chosenStudents.put(Color.values()[i], entranceStudents2.get(Color.values()[i]));
-            }
-            studentNumber2+= entranceStudents2.get(Color.values()[i]);
-            studentNumber2+= diningStudents.get(Color.values()[i]);
-            while(studentNumber2>2){
-                entranceStudents2.put(Color.values()[i],entranceStudents2.get(Color.values()[i])-1);
-                studentNumber2--;
-            }
-            while(studentNumber3>2){
-                diningStudents.put(Color.values()[i],diningStudents.get(Color.values()[i])-1);
-                studentNumber3--;
-            }
-            if(studentNumber2==2 && studentNumber3==2)
-                break;
-        }
-        cards[8].setChosenStudents(chosenStudents2,entranceStudentsSwap2);
-        cards[8].useCard(0,gm);
-        boolean check2=true;
-        for(Color c: chosenStudents2.keySet()){
-            if(gm.getPlayerByID(1).getEntryStudents().get(c)<chosenStudents2.get(c)){
-                check2=false;
-            }
-        }
-        for(Color c:entranceStudentsSwap2.keySet()){
-            if(gm.getPlayerByID(1).getStudents().get(c)<entranceStudentsSwap2.get(c)){
-                check2=false;
-            }
-        }
-        assertEquals(check2,true);
 
+    }
+
+    @Test
+    void testUseCard9(){
+        entranceStudents=gm.getPlayerByID(1).getEntryStudents();
+        EnumMap<Color,Integer>  diningStudents=gm.getPlayerByID(1).getStudents();
+        System.out.println(entranceStudents);
+        System.out.println(diningStudents);
+        int studentNumber=0;
+        int studentNumber2=0;
+        int i=0;
+
+        while(studentNumber<2) {
+            for (Color c : Color.values()) {
+                if (studentNumber < 2 && entranceStudents.containsKey(c) && (entranceStudents.get(c)-i) > 0) {
+                    entranceStudentsSwap.put(c, 1+i);
+                    studentNumber++;
+                }
+            }
+            i++;
+        }
+        i=0;
+        while(studentNumber2<2) {
+            for (Color c : Color.values()) {
+                if (studentNumber2 < 2 && diningStudents.containsKey(c) && (diningStudents.get(c)-i) > 0) {
+                    chosenStudents.put(c, 1+i);
+                    studentNumber2++;
+                }
+            }
+            i++;
+        }
+
+        System.out.println(entranceStudentsSwap);
+        System.out.println(chosenStudents);
+
+        cards[8].setChosenStudents(chosenStudents,entranceStudentsSwap);
+        cards[8].useCard(0,gm);
+        boolean check=true;
+        for(Color c: chosenStudents.keySet()){
+            if(gm.getPlayerByID(1).getEntryStudents().get(c)<chosenStudents.get(c)){
+                check=false;
+            }
+        }
+        for(Color c:entranceStudentsSwap.keySet()){
+            if(gm.getPlayerByID(1).getStudents().get(c)<entranceStudentsSwap.get(c)){
+                check=false;
+            }
+        }
+        assertEquals(check,true);
+
+    }
+
+    @Test
+    void testUseCard10(){
         //test effect of card 10
         for(Color c: Color.values()){
             chosenStudents.put(c,0);
@@ -320,6 +283,10 @@ class CharacterCardTest {
         cards[9].setChosenStudents(chosenStudents);
         cards[9].useCard(0,gm);
         assertEquals(gm.getPlayerByID(gm.getCurrentPlayer()).getStudentsOf(c),oldNumberStud+1);
+    }
+
+    @Test
+    void testUseCard11(){
         //test effect of card 11
         cards[10].setChosenColor(Optional.of(Color.BLUE));
         int numberBlueStud1=gm.getPlayerByID(1).getStudentsOf(Color.BLUE);
@@ -331,14 +298,17 @@ class CharacterCardTest {
         int key=gm.getNumberOfStudentBag();
         cards[10].useCard(0,gm);
         assertEquals(gm.getNumberOfStudentBag(),key+numberBlueStud2+numberBlueStud1);
+    }
+
+    @Test
+    void useCard12(){
         //test effect of card 12
         gm.setCurrentPlayer(2);
         gm.moveToSchool(1,Color.RED);
         gm.getPlayerByID(2).setStudents(Color.RED,gm.getPlayerByID(1).getStudentsOf(Color.RED));
         cards[11].useCard(0,gm);
         assertEquals(gm.getProfessors().get(Color.RED).getPlayer(),gm.getPlayerByID(2));
-
-    }*/
+    }
 
     @Test
     void testIncreaseCost() {
