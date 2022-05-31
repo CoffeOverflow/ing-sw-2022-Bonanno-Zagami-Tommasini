c6ac4415397c3b902679ab692fa486b3690b6186
Angelo Zagami <angelo.zagami@mail.polimi.it>
2022-05-31 21:41:10 +0200

    Deleted MainMenu.java, main menu is now launched by GUI.

--- src/main/java/it/polimi/ingsw/Client/GUI/GUI.java
+++ src/main/java/it/polimi/ingsw/Client/GUI/GUI.java
@@ -1,13 +1,54 @@
 package it.polimi.ingsw.Client.GUI;
 
-public class GUI implements Runnable
+import it.polimi.ingsw.Client.ServerHandler;
+import it.polimi.ingsw.Client.VirtualModel;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GUI extends Application implements Runnable
 {
+    private ServerHandler serverHandler;
+    private VirtualModel vmodel;
+    private Stage stage;
+    private Scene currentScene;
+
+    public GUI(){
+        this.vmodel = new VirtualModel();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //setup();
+        this.stage = stage;
+        Font.loadFont(getClass().getResourceAsStream("/fonts/newborough.ttf"), 14);
+        run();
+    }
+
     public static void main(String[] args) {
-        MainMenu.main(args);
+        launch(args);
     }
 
     @Override
     public void run() {
-
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        Scene sc = new Scene(root);
+        sc.getStylesheets().addAll(this.getClass().getResource("/css/mainmenu.css").toExternalForm());
+        stage.setScene(sc);
+        stage.sizeToScene();
+        stage.setResizable(false);
+        stage.setTitle("Eriantys");
+        stage.show();
     }
 }
