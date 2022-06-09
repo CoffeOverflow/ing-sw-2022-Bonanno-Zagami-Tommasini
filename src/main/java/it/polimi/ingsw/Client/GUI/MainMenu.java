2fa365e98865121cbc275b3e6c6130062acbc026
REPLYNET\gi.bonanno <gi.bonanno@reply.it>
2022-06-09 16:55:26 +0200

    boph

---
+++ src/main/java/it/polimi/ingsw/Client/GUI/MainMenu.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.Client.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class MainMenu extends Application
+{
+    // When using IntelliJ, don't run the application from here, use the main
+    // method in JavaFXMain
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Font.loadFont(getClass().getResourceAsStream("/fonts/newborough.ttf"), 14);
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        Scene sc = new Scene(root);
+        sc.getStylesheets().addAll(this.getClass().getResource("/css/mainmenu.css").toExternalForm());
+        primaryStage.setScene(sc);
+        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Eriantys");
+        primaryStage.show();
+
+    }
+}
